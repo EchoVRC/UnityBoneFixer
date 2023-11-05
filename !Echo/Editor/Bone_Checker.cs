e5a7cae754b3cefc5386e0efe288331d17d31a75
EchoVRC <me@lamfo.ru>
2023-11-05 14:43:00 +0300

    v1.0.0
    Uploading fully working code + description to it

---
+++ !Echo/Editor/Bone_Checker.cs
@@ -0,0 +1,130 @@
+﻿/***
+ *░░░░███████╗░██████╗██╗░░██╗░██████╗░██╗░░░██╗██████╗░░██████╗░░░
+ *░░░░██╔════╝██╔════╝██║░░██║██╔═══██╗██║░░░██║██╔══██╗██╔════╝░░░
+ *░░░░█████╗░░██║░░░░░███████║██║░░░██║██║░░░██║██████╔╝██║░░░░░░░░
+ *░░░░██╔══╝░░██║░░░░░██╔══██║██║░░░██║╚██╗░██╔╝██╔══██╗██║░░░░░░░░
+ *░░░░███████╗╚██████╗██║░░██║╚██████╔╝░╚████╔╝░██║░░██║╚██████╗░░░
+ *░░░░╚══════╝░╚═════╝╚═╝░░╚═╝░╚═════╝░░░╚═══╝░░╚═╝░░╚═╝░╚═════╝░░░
+*/
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+public class SkinnedMeshBoneCheckerEditor : Editor
+{
+    // Добавление нового пункта в контекстное меню GameObject.
+    [MenuItem("GameObject/Check Skinned Mesh Bones", false, 10)]
+    static void CheckSkinnedMeshBonesInChildren(MenuCommand menuCommand)
+    {
+        // Получение выбранного объекта в иерархии.
+        GameObject selectedGameObject = Selection.activeGameObject;
+        if (selectedGameObject != null)
+        {
+            // Проверка костей у объекта и всех его потомков.
+            CheckBonesInAllChildren(selectedGameObject.transform);
+        }
+        else
+        {
+            // Вывод сообщения в консоль, если объект не выбран.
+            Debug.Log("No object selected in the hierarchy.");
+        }
+    }
+
+    // Рекурсивная функция для проверки всех потомков переданного transform.
+    static void CheckBonesInAllChildren(Transform parent)
+    {
+        // Перебор всех потомков текущего transform.
+        foreach (Transform child in parent)
+        {
+            // Попытка получить компонент SkinnedMeshRenderer у потомка.
+            SkinnedMeshRenderer skinnedMeshRenderer = child.GetComponent<SkinnedMeshRenderer>();
+            if (skinnedMeshRenderer != null)
+            {
+                // Проверка костей у найденного SkinnedMeshRenderer.
+                CheckBones(skinnedMeshRenderer);
+            }
+            // Если у потомка есть свои потомки, рекурсивно вызываем проверку на них.
+            if (child.childCount > 0)
+            {
+                CheckBonesInAllChildren(child);
+            }
+        }
+    }
+
+    // Функция для проверки костей у SkinnedMeshRenderer.
+    static void CheckBones(SkinnedMeshRenderer skinnedMesh)
+    {
+        // Перебор всех костей в SkinnedMeshRenderer.
+        foreach (Transform bone in skinnedMesh.bones)
+        {
+            // Проверка на null каждой кости.
+            if (bone == null)
+            {
+                // Вывод ошибки в консоль, если обнаружена отсутствующая кость.
+                Debug.LogError("Missing bone detected in '" + skinnedMesh.gameObject.name + "'");
+                return;
+            }
+        }
+        // Вывод сообщения об успешной проверке, если все кости на месте.
+        Debug.Log("All bones are intact in '" + skinnedMesh.gameObject.name + "'");
+    }
+}
+
+
+public class SkeletonFixer : MonoBehaviour
+{
+    // Пункт меню для исправления костей в SkinnedMeshRenderer
+    [MenuItem("GameObject/Fix Skinned Mesh Bones in Children", false, 10)]
+    static void FixSkinnedMeshBonesInChildren(MenuCommand menuCommand)
+    {
+        // Получаем выбранный объект
+        GameObject selectedObject = Selection.activeGameObject;
+        if (selectedObject == null) return;
+
+        // Применяем исправление к каждому SkinnedMeshRenderer в дочерних объектах
+        foreach (SkinnedMeshRenderer skinnedMeshRenderer in selectedObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            RemoveBrokenBones(skinnedMeshRenderer);
+        }
+
+        // Выводим сообщение о завершении операции
+        Debug.Log("Skinned Mesh Bones fixing process is completed.");
+    }
+
+    // Функция для удаления поврежденных костей и замены их на пустышки
+    static void RemoveBrokenBones(SkinnedMeshRenderer skinnedMeshRenderer)
+    {
+        // Проверяем наличие костей и корневой кости
+        if (skinnedMeshRenderer.bones == null || skinnedMeshRenderer.rootBone == null) return;
+
+        Transform[] bones = skinnedMeshRenderer.bones;
+        bool hasMissingBones = false;
+        for (int i = 0; i < bones.Length; i++)
+        {
+            // Проверяем отсутствие кости
+            if (bones[i] == null)
+            {
+                hasMissingBones = true;
+                // Создаем новую кость-заглушку
+                GameObject fakeBone = new GameObject($"fake_{i}");
+                // Родитель для заглушки - корневая кость
+                fakeBone.transform.SetParent(skinnedMeshRenderer.rootBone);
+                // Позиционируем заглушку
+                fakeBone.transform.localPosition = Vector3.zero;
+                fakeBone.transform.localRotation = Quaternion.identity;
+                // Заменяем отсутствующую кость на заглушку
+                bones[i] = fakeBone.transform;
+            }
+        }
+
+        // Обновляем массив костей, если были найдены и заменены отсутствующие кости
+        if (hasMissingBones)
+        {
+            skinnedMeshRenderer.bones = bones;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# UnityBoneFixer
+# Unity Bone Fixer
+
+This collection of Unity Editor tools provides essential functionalities for checking and fixing SkinnedMeshRenderer components in 3D models. 
+
+Do you know the situation when FBX Exporter can't export a model?
+Well, this script fixes it.
+
+## Features
+
+### Skinned Mesh Bone Checker
+- **Recursive Bone Checking**: Checks all SkinnedMeshRenderers in the selected GameObject and its children.
+- **Immediate Diagnosis**: Logs results instantly in the Unity Console to inform if there are missing bone references.
+
+### Unity Skeleton Fixer
+- **Automatic Bone Repair**: Detects and replaces missing bones with placeholder objects for all SkinnedMeshRenderers within the selected GameObject's children.
+- **Non-Destructive**: Ensures the visual appearance of the mesh is not changed.
+
+## Installation
+
+1. Download last unitypackage: [UnityBoneFixer Releases](https://github.com/EchoVRC/UnityBoneFixer/releases)
+2. **OR** Clone/download the repository.
+
+## Usage
+
+### Checking Bones
+1. Select a GameObject in the Unity Editor's hierarchy view.
+2. Right-click and choose `Check Skinned Mesh Bones` from the GameObject menu.
+3. Review the Unity Console for messages about the integrity of the bones.
+
+### Fixing Bones
+1. Select the GameObject with SkinnedMeshRenderer components in the Unity Editor.
+2. Navigate to `GameObject` in the menu bar.
+3. Click on `Fix Skinned Mesh Bones in Children`.
+4. A message in the Unity Console will confirm the completion of the operation.
+
+## How It Works
+
+### Skinned Mesh Bone Checker
+- Triggers a recursive search through the selected GameObject's hierarchy.
+- Checks each SkinnedMeshRenderer's bones array for null references.
+- Reports back in the console about the status of each SkinnedMeshRenderer's bones.
+
+### Unity Skeleton Fixer
+- Scans for all SkinnedMeshRenderers within the children of the selected GameObject.
+- Creates placeholder bones for any missing bone references and properly parents them under the root bone.
+- Updates the bones array in SkinnedMeshRenderer to reference new placeholder bones.
+
+## Contribution
+
+Feel free to fork the repository, make changes, and submit a pull request if you have improvements or fixes to the tools. 
+
+## Support
+
+If you have any issues or questions about these tools, please file an issue in the GitHub repository.
+
+## License
+
+These tools are distributed under the MIT License. 
